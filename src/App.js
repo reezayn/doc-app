e70f299a8f8e6fba8b6844b57a45d6bda898008a
Rijan Shrestha <rijanshrestha98450@gmail.com>
2023-01-23 08:32:38 +0545

    done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import About from './components/About';
-import AllInOne from './components/AllInOne';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Footer from './components/Footer';
-import Hero from './components/Hero';
 import Navbar from './components/Navbar'
-import Pricing from './components/Pricing';
-import Support from './components/Support'
+// import About from './components/About';
+// import AllInOne from './components/AllInOne';
+// import Hero from './components/Hero';
+// import Pricing from './components/Pricing';
+// import Support from './components/Support'
+// import Patient from './components/Patient'
+import Homepage from "./pages/Homepage";
+import Patientpage from "./pages/Patientpage";
+import Doctorlistpage from "./pages/Doctorlistpage";
+import Pharmacypage from "./pages/Pharmacypage";
+// import Chatpage from "./pages/Chatpage";
+// import Signuppage from "./pages/Signuppage";
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Navbar />
-      <Hero />
-      <About />
-      <Support />
-      <AllInOne />
-      <Pricing />
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/pharmacy' element={<Pharmacypage />} />
+        {/* <Route path='/chat' element={<Chatpage />} /> */}
+        {/* <Route path='/signuppage' element={<Signuppage />} /> */}
+        <Route path='/patientpage' element={<Patientpage />} />
+        <Route path='/patientpage/doctorlistpage' element={<Doctorlistpage />} />
+      </Routes>
       <Footer />
+    </BrowserRouter>
     </>
   );
 }
